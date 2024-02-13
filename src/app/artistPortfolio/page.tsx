db9e5065eb4b1999e00e367185bb85ac7f7ac8b1
Sumiyaakhi <sumiya.akhi793@gmail.com>
2024-02-14 00:47:10 +0600

    navbar added

---
+++ src/app/artistPortfolio/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArtistPortfolio = () => {
+    return (
+        <div>
+            This is Artist portfolio page
+        </div>
+    );
+};
+
+export default ArtistPortfolio;
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div>
+            This is the gallery page
+        </div>
+    );
+};
+
+export default Gallery;
